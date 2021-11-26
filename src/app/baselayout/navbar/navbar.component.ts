7505382aba0dad6b2956b82fd8c1100290c9aeb6
Shiv Raj Bhagat <shivraj-bhagat@gmail.com>
2021-11-26 12:09:44 +0530

    2nd standup and two layout working

--- src/app/baselayout/navbar/navbar.component.ts
+++ src/app/baselayout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { GlobalVariablesService } from 'src/app/services/global-variables.service';
 
 @Component({
@@ -9,16 +10,35 @@ import { GlobalVariablesService } from 'src/app/services/global-variables.servic
 export class NavbarComponent implements OnInit {
 
     sideBarStatus: boolean = false;
-    constructor(private globalVariable: GlobalVariablesService) {}
+    constructor(private globalVariable: GlobalVariablesService, 
+        @Inject(DOCUMENT) private document: Document,
+        private renderer: Renderer2,) {}
 
     ngOnInit(): void {
         this.globalVariable.sideBarVisibility.subscribe(data => {
             this.sideBarStatus = data
         })
+        this.onShowSidebar()
     }
 
     toggle() {
         this.sideBarStatus = !this.sideBarStatus
         this.globalVariable.sideBarVisibility.next(this.sideBarStatus)
+        this.onShowSidebar()
     }
+
+    onShowSidebar() {
+        if(this.sideBarStatus) this.renderer.setAttribute(this.document.body, 'sidebar-status', 'shown');
+        if(!this.sideBarStatus) this.renderer.setAttribute(this.document.body, 'sidebar-status', 'hidden');
+    }
+
+    toggleLayout() {
+        let layout = this.document.getElementsByTagName("body")[0].getAttribute("layout");
+        if(layout == "first") {
+            this.renderer.setAttribute(this.document.body,"layout","second")
+        } else {
+            this.renderer.setAttribute(this.document.body,"layout","first")
+        }
+    }
+
 }
